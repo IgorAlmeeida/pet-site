0c372b9d8492fdbb1ac67fd66f10c6085a6cdb35
ShiroeTwo <igoralmeeida@hotmail.com>
2020-11-16 22:04:35 -0300

    editando menu

--- petsite/adminpet/urls.py
+++ petsite/adminpet/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
-from .views import home
+from .views import home, list_projects, new_project, update_project, delete_project, list_my_projects
 
 urlpatterns = [
     path('', home, name='page_home'),
+    path('projeto/',list_projects, name='list_project'),
+    path('projeto/my/', list_my_projects, name='list_my_project'),
+    path('projeto/new',new_project, name='new_project'),
+    path('projeto/update/<id>/',update_project,name='update_project'),
+    path('projeto/delete/<id>/',delete_project, name='delete_project'),
+
 ]
--- petsite/adminpet/views.py
+++ petsite/adminpet/views.py
@@ -5,3 +5,19 @@ from django.http import HttpResponse
 
 def home (request):
     return render(request,'adminpet/index.html')
+
+def list_projects(request):
+    return ''
+
+def new_project(request):
+    return ''
+
+def update_project(resquest, id):
+    return ''
+
+def delete_project(request, id):
+    return ''
+
+def list_my_projects(request):
+    return ''
+    

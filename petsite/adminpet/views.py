ca6faad3b9714da15f99dc22090f2e6d4d404a91
ShiroeTwo <igoralmeeida@hotmail.com>
2020-11-21 18:52:36 -0300

    create and list projects

--- petsite/adminpet/views.py
+++ petsite/adminpet/views.py
@@ -1,23 +1,37 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import ProjectForm
+from .models import Project
 
 # Create your views here.
 
 def home (request):
     return render(request,'adminpet/index.html')
 
-def list_projects(request):
-    return ''
+def listProjects(request):
+    projects = Project.objects.all()
+    data = {'projects': projects}
+    return render(request, 'adminpet/list_project.html', data)
 
-def new_project(request):
-    return ''
+def listMyProjects(request):
+    return render(request, 'adminpet/list_my_projects.html')
 
-def update_project(resquest, id):
-    return ''
+def newProject(request):
+    form = ProjectForm(request.POST or None)
 
-def delete_project(request, id):
-    return ''
+    if(request.method == 'POST'):
+        if(form.is_valid()):
+            form.save()
+            return redirect('list_project')
+    else:
+        formProject = ProjectForm()
+        data = {'form_project': formProject}
+        return render(request, 'adminpet/new_project.html', data)
 
-def list_my_projects(request):
-    return ''
+def updateProject(resquest, id):
+    render(request, 'adminpet/update_project.html')
+
+def deleteProject(request, id):
+    render(request, 'adminpet/delete_project.html')
+
     
--- petsite/petsite/settings.py
+++ petsite/petsite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'bolsa',
     'adminpet',
     'website',
+    'bootstrapform',
 ]
 
 MIDDLEWARE = [

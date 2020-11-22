1cdc22c046fe026ad7750e9622459256d38f4b6d
ShiroeTwo <igoralmeeida@hotmail.com>
2020-11-22 19:01:05 -0300

    resolving erros

--- petsite/petsite/settings.py
+++ petsite/petsite/settings.py
@@ -79,8 +79,9 @@ WSGI_APPLICATION = 'petsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 default_dburl = 'sqlite:///'+os.path.join(BASE_DIR, 'db.sqlite3')
+
 DATABASES = {
-    config('DATABASE_URL', default=default_dburl, cast=dburl),
+    'default':config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 

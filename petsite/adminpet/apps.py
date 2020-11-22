2a92535615b9e4f9145d271ac127c5bc6edd8cd9
ShiroeTwo <igoralmeeida@hotmail.com>
2020-11-22 19:11:41 -0300

    tesing

--- petsite/adminpet/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminpetConfig(AppConfig):
-    name = 'adminpet'
--- petsite/bolsa/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BolsaConfig(AppConfig):
-    name = 'bolsa'

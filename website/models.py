874e6e50c8b5f083930eb099c48586fc08c1df95
ShiroeTwo <igoralmeeida@hotmail.com>
2020-11-28 13:32:12 -0300

    creating models blog

--- website/models.py
+++ website/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from adminpet.models import Profile
 
 # Create your models here.
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    course = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+
+class Post(models.Model):
+    title = models.CharField(max_length=300, blank=False, null=False)
+    dateCreate = models.DateTimeField(auto_now=True, blank=False, null=False)
+    dateUpdated = models.DateTimeField(auto_now= True)
+    body = models.TextField(blank=False, null=False)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    creator = models.ForeignKey(Profile, on_delete=models.DO_NOTHING) 
+
+
+    

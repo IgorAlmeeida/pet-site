8715176060d9b1425c49d6064073b31d365c169a
igorAlmeeida <igoralmeeida@hotmail.com>
2020-12-01 00:23:10 -0300

    creating list post and blog

--- website/views.py
+++ website/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def home(request):
+
     return render(request, 'website/index.html')
 
-def pageBlog(request): 
-    return render(request, 'website/blog.html')
+def pageBlog(request):
+    posts = Post.objects.all()
+    data = {}
+    data['posts'] = posts 
+    return render(request, 'website/blog.html', data)
 
 def pageMembers(request):
     return render(request, 'website/membros.html')
@@ -18,7 +23,10 @@ def pageContatus(request):
     return render(request, 'website/contato.html')
 
 def pagePost(request, idPost):
-    return render(request, 'website/post.html')
+    post = Post.objects.get(id=idPost)
+    data = {}
+    data['post'] = post
+    return render(request, 'website/post.html', data)
 
 def pageProject(request, idProject):
     return render(request, 'website/projetos.html')
